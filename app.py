4c16eee61f9c4d2d1f4280f05301c7764e57b4bc
Jacob Baldwin <jacobmbaldwin@gmail.com>
2024-02-25 19:01:33 -0600

    make it better

--- app.py
+++ app.py
@@ -1,31 +1,23 @@
-import os
-import json
 import requests
+import json
+import os
 
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
-
-from flask import FLask, request
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def webhook():
-    data = request.get_json()
-
-    if data['name'] != 'Safety Bot Test':
-        msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-        send_message(msg)
-
-    return "ok", 200
+url = "https://api.groupme.com/v3/groups?token=4RBEXfcfeke2TByTMwviZJQDo7ebLuwTr2puwiLe"
+response = requests.get(url)
+data = response.json()
+print(response.status_code)
 
 
 def send_message(msg):
-    url = 'https://api.groupme.com/v3/bots/post'
+    bot_url = 'https://api.groupme.com/v3/bots/post'
 
     data = {
         'bot_id': os.getenv('GROUPME_BOT_ID'),
         'text': msg,
     }
-    response = requests.post(url, json=data)
-    x = urlopen(response).read().decode()
+    message = requests.post(url, json=data)
+
+
+if response['name'] != 'Safety Bot Test':
+    msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+    send_message(msg)
